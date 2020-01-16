5628806399e41beedb0e5dfc159d068c311c8d90
calebanthony <caleb@datajoe.com>
2020-01-16 10:36:05 -0700

    Make navbar background full width

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,32 +1,30 @@
 <template>
   <div id="app">
-    <div
-      class="nav level is-mobile"
-      role="navigation"
-      aria-label="primary navigation"
-    >
-      <div class="level-left">
-        <Logo v-if="showLogo" />
-      </div>
-      <div class="level-right">
-        <a class="level-item" href="//github.com/calebanthony/">
-          <span class="icon is-medium">
-            <fa :icon="['fab', 'github']" size="2x" />
-          </span>
-        </a>
-        <a
-          class="level-item"
-          href="//stackoverflow.com/users/6646558/caleb-anthony?tab=profile"
-        >
-          <span class="icon is-medium">
-            <fa :icon="['fab', 'stack-overflow']" size="2x" />
-          </span>
-        </a>
-        <a class="level-item" href="//www.linkedin.com/in/calebanthony/">
-          <span class="icon is-medium">
-            <fa :icon="['fab', 'linkedin-in']" size="2x" />
-          </span>
-        </a>
+    <div class="nav-wrapper" role="navigation" aria-label="primary navigation">
+      <div class="nav level is-mobile">
+        <div class="level-left">
+          <Logo v-if="showLogo" />
+        </div>
+        <div class="level-right">
+          <a class="level-item" href="//github.com/calebanthony/">
+            <span class="icon is-medium">
+              <fa :icon="['fab', 'github']" size="2x" />
+            </span>
+          </a>
+          <a
+            class="level-item"
+            href="//stackoverflow.com/users/6646558/caleb-anthony?tab=profile"
+          >
+            <span class="icon is-medium">
+              <fa :icon="['fab', 'stack-overflow']" size="2x" />
+            </span>
+          </a>
+          <a class="level-item" href="//www.linkedin.com/in/calebanthony/">
+            <span class="icon is-medium">
+              <fa :icon="['fab', 'linkedin-in']" size="2x" />
+            </span>
+          </a>
+        </div>
       </div>
     </div>
 
